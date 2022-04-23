4688f0f100f1305b47d85703a20ea50c732c1853
mojisilva <mojiasilva@gmail.com>
2022-04-23 10:05:15 -0300

    creation images files, css, js and index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+function start(){
+
+    $("#inicio").hide();
+    $("#fundo-game").append("<div class='anima1' id='jogador'></div>")
+    $("#fundo-game").append("<div class='anima2' id='inimigo1'></div>")
+    $("#fundo-game").append("<div id='inimigo2'></div>")
+    $("#fundo-game").append("<div class='anima3' id='amigo'></div>")
+}
+
